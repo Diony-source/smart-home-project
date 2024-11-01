47403919ded5eca2ed22d964f17fc07dbe538e87
Diony-source <erendalkiran2001@gmail.com>
2024-11-01 17:12:48 +0300

    Moved the jwt key to .env and updated it to be initialized in main.go.

--- main.go
+++ main.go
@@ -1,33 +1,35 @@
 package main
 
 import (
-	"net/http"
-	"smart-home-project/api"
-	"smart-home-project/repositories"
+    "net/http"
+    "smart-home-project/api"
+    "smart-home-project/repositories"
+    "smart-home-project/utils"
 
-	"github.com/joho/godotenv"
-	"github.com/sirupsen/logrus"
+    "github.com/joho/godotenv"
+    "github.com/sirupsen/logrus"
 )
 
 func main() {
-	// Log yapılandırması
-	logrus.SetFormatter(&logrus.JSONFormatter{})
-	logrus.SetLevel(logrus.InfoLevel)
+    logrus.SetFormatter(&logrus.JSONFormatter{})
+    logrus.SetLevel(logrus.InfoLevel)
 
-	logrus.Info("Starting Smart Home API server...")
+    logrus.Info("Starting Smart Home API server...")
 
-	err := godotenv.Load()
-	if err != nil {
-		logrus.Warn("No .env file found, using default environment variables")
-	}
+    err := godotenv.Load()
+    if err != nil {
+        logrus.Warn("No .env file found, using default environment variables")
+    }
 
-	err = repositories.ConnectDatabase()
-	if err != nil {
-		logrus.Fatal("Failed to connect to database:", err)
-	}
+    utils.InitJWTKey()
 
-	router := api.Router()
+    err = repositories.ConnectDatabase()
+    if err != nil {
+        logrus.Fatal("Failed to connect to database:", err)
+    }
 
-	logrus.Info("Smart Home API server is running on port 8080")
-	http.ListenAndServe(":8080", router)
+    router := api.Router()
+
+    logrus.Info("Smart Home API server is running on port 8080")
+    http.ListenAndServe(":8080", router)
 }
--- utils/jwt.go
+++ utils/jwt.go
@@ -1,32 +1,42 @@
 package utils
 
 import (
-	"errors"
-	"time"
+    "errors"
+    "os"
+    "time"
 
-	"github.com/dgrijalva/jwt-go"
+    "github.com/dgrijalva/jwt-go"
 )
 
-var jwtKey = []byte("your_secret_key")
+var jwtKey []byte
+
+// JWT Anahtarını .env Dosyasından Alır
+func InitJWTKey() {
+    key := os.Getenv("JWT_SECRET_KEY")
+    if key == "" {
+        panic("JWT_SECRET_KEY is not set in environment variables")
+    }
+    jwtKey = []byte(key)
+}
 
 // JWT Token oluşturur
 func GenerateJWT(username, role string) (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"username": username,
-		"role":     role,
-		"exp":      time.Now().Add(time.Hour * 24).Unix(),
-	})
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+        "username": username,
+        "role":     role,
+        "exp":      time.Now().Add(time.Hour * 24).Unix(),
+    })
 
-	return token.SignedString(jwtKey)
+    return token.SignedString(jwtKey)
 }
 
 func ParseJWT(tokenString string) (*jwt.Token, error) {
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, errors.New("unexpected signing method")
-		}
-		return jwtKey, nil
-	})
+    token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+        if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+            return nil, errors.New("unexpected signing method")
+        }
+        return jwtKey, nil
+    })
 
-	return token, err
+    return token, err
 }
